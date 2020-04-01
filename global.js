653269977d21cd010b18eaf5ade9ad940e141a51
fbarda <arda.aydin@operationsilkscarf.com>
2020-04-01 20:35:50 +0300

    Write proper JSDoc

--- global.js
+++ global.js
@@ -1,4 +1,23 @@
+//jshint -W098
+/**@module ForkedOutPromise:globalDoc
+ * @author fbarda arda.aydin@operationsilkscarf.com
+ */
+/**@callback PromiseReject
+ * @param {Error|String} rejectReason - The error to throw via the promise.
+ * @returns {void}
+ */
+/**@callback PromiseResolve
+ * @param {*} resolveValue - The value to resolve the promise.
+ * @returns {void}
+ */
+/**@typedef {Object} ForkedOutPromiseInstance - An object containing a promise and the promise's resolve and reject functions.
+ * @prop {Promise<*>} promise - Designed to return via a callback-style async function. Use `this.resolve` and `this.reject` to control the promise via a callback.
+ * @prop { PromiseResolve } resolve Makes the [promise]{@link ForkedOutPromiseInstance#promise} resolve with the given argument.
+ * @prop { PromiseReject } reject Makes the [promise]{@link ForkedOutPromiseInstance#promise} reject with the given error argument.
+*/
+/** @class */
 class ForkedOutPromise {
+    /** @returns {ForkedOutPromiseInstance} A new {@linkplain ForkedOutPromise} object. */
     constructor() {
         const classThis = this;
         this.promise = new Promise(
